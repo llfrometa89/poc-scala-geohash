da8cda1cf4f6829384db53d526318e7b03f18d8a
Livan Frometa <llfrometa@gmail.com>
2022-08-18 00:19:15 +0200

    Solve main function issues

--- src/main/scala/com/stuart/geohash/Main.scala
+++ src/main/scala/com/stuart/geohash/Main.scala
@@ -1,24 +1,8 @@
 package com.stuart.geohash
 
 import cats.effect.{IO, IOApp}
-import cats.implicits._
-import ch.hsr.geohash.GeoHash
 
 object Main extends IOApp.Simple {
 
-  override def run: IO[Unit] = for {
-    a <- IO.delay(GeoHash.fromGeohashString("sp3e3"))
-    _ <- IO(
-      println(s"Hello from GeoHash: ${a.getOriginatingPoint.getLatitude}")
-    ) >> IO(
-      println(s"Hello GeoHash: ${GeoHash.geoHashStringWithCharacterPrecision(41.388828145321, 2.1689976634898, 12)}")
-    ) >>
-      IO(
-        println(s"Hello GeoHash: ${GeoHash.geoHashStringWithCharacterPrecision(41.388828145321, 2.1689976634898, 5)}")
-      ) >> IO(
-        println(s"Hello GeoHash: ${GeoHash.geoHashStringWithCharacterPrecision(41.390743, 2.138177, 8)}")
-      ) >> IO(
-        println(s"Hello GeoHash: ${GeoHash.geoHashStringWithCharacterPrecision(41.390853, 2.138067, 8)}")
-      )
-  } yield ()
+  override def run: IO[Unit] = ???
 }
